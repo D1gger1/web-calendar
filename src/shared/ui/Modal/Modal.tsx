d4d181dcd4bb2babc721bd9aede7c5e1011dc903
Max <conr9811@gmail.com>
2026-02-21 10:31:16 +0100

    Remove commented close button from Modal component

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { ModalProps } from "./Modal.types";
 import styles from "./Modal.module.scss";
-import closeBtn from '../../../assets/closeBtn.svg';
+
 
 export const Modal: React.FC<ModalProps> = ({
     isOpen,
@@ -17,16 +17,6 @@ export const Modal: React.FC<ModalProps> = ({
                 className={`${styles.modalContent} ${className ?? ''}`}
                 onClick={(e) => e.stopPropagation()}
             >
-                {/* <div className={styles.header}>
-                    <button
-                        onClick={onClose}
-                        className={styles.closeButton}
-                        aria-label="Close modal"
-                    >
-                        <img src={closeBtn} alt="" className={styles.closeIcon} />
-                    </button>
-                </div> */}
-
                 {children}
             </div>
         </div>
